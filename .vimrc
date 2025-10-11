a1312e3ed3bc7c49ac6f6d4047b8740c570dd75d
Jeremías <jrmsdev@gmail.com>
2025-10-11 11:24:59 -0300

    install files initial commit

--- .vimrc
+++ .vimrc
@@ -1,4 +1,13 @@
+" jrmsdev/dotfiles .vimrc
+
 syntax on
 set number
+
 colorscheme desert
 set bg=dark
+
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set noexpandtab
+set smartindent
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eu
+
+umask 0027
+
+PATH=/usr/bin:/bin
+DIR_MODE=0750
+FILE_MODE=0640
+BUP_TSTAMP=$(date '+%Y%m%d-%H%M%S')
+
+PREFIX=${PREFIX:-${HOME}}
+
+install_file() {
+	local filename=$1
+	local src=${PWD}/${filename}
+	local dst=${PREFIX}/${filename}
+	local bup=${filename}.${BUP_TSTAMP}
+	grep -F "jrmsdev/dotfiles ${filename}" "${src}" >/dev/null || {
+		echo "ERROR: jrmsdev/dotfiles ${filename} - install not enabled" >&2
+		return 9
+	}
+	install -v -C -m "${FILE_MODE}" -b -B "${bup}" "${src}" "${dst}"
+	return 0
+}
+
+echo "Source: ${PWD}"
+echo "Dest: ${PREFIX}"
+
+install -v -d -m "${DIR_MODE}" "${PREFIX}"
+
+install_file .vimrc
+
+exit 0
