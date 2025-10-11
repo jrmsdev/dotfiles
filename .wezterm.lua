97e7e2935b8028eee9a947982747bb2500c5bbbc
Jeremías <jrmsdev@gmail.com>
2025-10-11 10:43:41 -0300

    term visual bell (except for rio)
    rio has sound bell hardcoded so can't be disable, so disable visual
    for rio, stay with sound until it can be disabled, as configured.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 
 return {
+  --default_gui_startup_args = { "start", "--maximize" },
   enable_tab_bar = false,
   color_scheme = "Builtin",
   colors = {
@@ -8,6 +9,7 @@ return {
     background = "#111111",
     cursor_bg = "#cccccc",
     cursor_border = "#cccccc",
+    visual_bell = "#333333",
   },
   font = wezterm.font("Monaco"),
   font_size = 18.0,
@@ -17,5 +19,12 @@ return {
     right = 0,
     top = 0,
     bottom = 0,
+  },
+  audible_bell = "Disabled",
+  visual_bell = {
+    fade_in_function = "EaseIn",
+    fade_in_duration_ms = 30,
+    fade_out_function = "EaseOut",
+    fade_out_duration_ms = 30,
   },
 }
