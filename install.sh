1dfac0444b920fde44e1e829484e477affd43e3a
Jeremías <jrmsdev@gmail.com>
2025-10-11 22:39:06 -0300

    install source check

--- install.sh
+++ install.sh
@@ -10,11 +10,22 @@ BUP_TSTAMP=$(date '+%Y%m%d-%H%M%S')
 
 PREFIX=${PREFIX:-${HOME}}
 
+install_source_check() {
+	local filename=$1
+	local src=${PWD}/${filename}
+	grep -F "jrmsdev/dotfiles ${filename}" "${src}" >/dev/null || {
+		echo "ERROR: jrmsdev/dotfiles ${filename} - install not enabled" >&2
+		return 9
+	}
+	return 0
+}
+
 install_file() {
 	local filename=$1
 	local src=${PWD}/${filename}
 	local dst=${PREFIX}/${filename}
 	local bup=${filename}.${BUP_TSTAMP}
+	install_source_check "${filename}"
 	grep -F "jrmsdev/dotfiles ${filename}" "${src}" >/dev/null || {
 		echo "ERROR: jrmsdev/dotfiles ${filename} - install not enabled" >&2
 		return 9
